5e18fb1d3bf709f77c9e5b354ace2bf9ae7b8046
madhu arigela <madhusimhadri955@gmail.com>
2024-03-14 10:26:46 +0530

    onlize quiz system set up

---
+++ madhuvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\madhu\onedrive\desktop\onlinequizesystem-main\madhuvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
